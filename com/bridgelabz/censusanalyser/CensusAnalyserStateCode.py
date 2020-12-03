6e7474cf17797e332f878a154ac2e658a1e78776
Archana-Bhamare <archusonar2@gmail.com>
2020-12-03 10:20:32 +0530

    Added : state code census headers

---
+++ com/bridgelabz/censusanalyser/CensusAnalyserStateCode.py
@@ -0,0 +1,8 @@
+class CensusAnalyserStateCode:
+    def __init__(self):
+        self.state = "State"
+        self.state_code = "State-Code"
+
+    def __repr__(self):
+        return self.state + "," + self.state_code
+
