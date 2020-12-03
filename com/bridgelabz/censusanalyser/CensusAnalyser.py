4634800e9f8c590650e6f3eaef1c8254e7fa9a14
Archana-Bhamare <72537658+Archana-Bhamare@users.noreply.github.com>
2020-12-03 10:32:54 +0530

    Merge

--- com/bridgelabz/censusanalyser/CensusAnalyser.py
+++ com/bridgelabz/censusanalyser/CensusAnalyser.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from com.bridgelabz.censusanalyser.CensusAnalyserStateCode import CensusAnalyserStateCode
 from com.bridgelabz.censusanalyser.CensusAnalysisException import CensusAnalysisException
+from com.bridgelabz.censusanalyser.CensusAnalysisHeader import CensusAnalysisHeader
 
 
 class CensusAnalyzer:
 
     def __init__(self, path):
         self.path = path
+        
+    def record_counter(self):
+        """
 
+        :return: number of records in file
+        """
+        try:
+            col_names = repr(CensusAnalysisHeader()).split(",")
+            data = pd.read_csv(self.path, usecols=col_names)
+            return len(data)
+        except FileNotFoundError:
+            raise CensusAnalysisException("Invalid File Path")
+        except ValueError:
+            raise CensusAnalysisException("Wrong Delimiter or Invalid Columns Name")
+    
     def load_state_code(self):
         """
         :return: number of records in file
@@ -23,5 +38,8 @@ class CensusAnalyzer:
 
 
 if __name__ == "__main__":
+    read_csv = CensusAnalyzer(path="C:/Users/KatK/PycharmProjects/StateCensusAnalyser/Data/IndiaStateCensusData.csv")
+    length = read_csv.record_counter()
+    print(length)
     read_statecode = CensusAnalyzer(path="C:/Users/KatK/PycharmProjects/StateCensusAnalyser/Data/IndiaStateCode.csv")
     print(read_statecode.load_state_code())
