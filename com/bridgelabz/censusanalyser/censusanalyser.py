d3e5ff5f46bcfbbbb90c786eaacf0a76fb3bd146
Archana-Bhamare <archusonar2@gmail.com>
2020-12-04 17:47:46 +0530

    Refactored : Refactor the code as DRY principle

---
+++ com/bridgelabz/censusanalyser/censusanalyser.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from com.bridgelabz.censusanalyser.CensusAnalysisException import CensusAnalysisException
+from com.bridgelabz.censusanalyser.CensusAnalysisHeader import CensusAnalysisHeader
+from com.bridgelabz.censusanalyser.CensusAnalyserStateCode import CensusAnalyserStateCode
+
+
+class CensusAnalyzer:
+
+    def __init__(self, path, obj):
+        self.path = path
+        self.obj = obj
+
+    def record_counter(self):
+        """
+        :return: number of records in file
+        """
+        try:
+            col_names = repr(self.obj).split(",")
+            data = pd.read_csv(self.path, usecols=col_names)
+            return len(data)
+        except FileNotFoundError:
+            raise CensusAnalysisException("Invalid File Path")
+        except ValueError:
+            raise CensusAnalysisException("Wrong Delimiter or Invalid Columns Name")
